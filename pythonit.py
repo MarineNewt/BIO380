c0fca028080123a006da0bbf76d8f95b39024253
Frankie Leyva <frankieiii15@comcast.net>
2022-04-28 18:25:44 -0700

    Add Python file

---
+++ pythonit.py
@@ -0,0 +1,5 @@
+#! /usr/bin/env python3
+
+##This File will call the appropriate files to get data related to the graph that we intend to create.
+## Then produce graphs to best represent that data to asnwer our target questions.
+
