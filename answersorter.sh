9208f5c85b373561c98023e2720162b3419d80c5
Frankie Leyva <frankieiii15@comcast.net>
2022-05-04 02:23:38 -0700

    rework answersorter & add outputfiles
    reworked answersorter.sh to produce individual .txt files for each question

---
+++ answersorter.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Frankie Leyva
+#BIO380 Spring 22
+#5/03/22
+#Assignment 4
+
+#
+
+#
+for column in {1..119}
+do
+title=$(cut -d "," -f $column nondata.txt | head -n 1)
+t="$title".txt""
+echo $t
+cut -d "," -f 1 nondata.txt > $t
+done
+
